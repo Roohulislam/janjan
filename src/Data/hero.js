85e3f6ff37f2d397f76a71dda16246acfec57374
rohail jan <rohailjan726@gmail.com>
2026-06-25 10:35:45 +0500

    Add hero section with Cloudinary video

--- src/Data/hero.js
+++ src/Data/hero.js
@@ -1,173 +1,211 @@
-// Replace these with your actual image imports
-import hero1 from "../assets/hero/1a.png";
-import hero2 from "../assets/hero/2a.png";
-import hero3 from "../assets/hero/3a.png";
-import hero4 from "../assets/hero/4a.png";
-import hero5 from "../assets/hero/5a.png";
-import hero6 from "../assets/hero/6a.png";
-import hero7 from "../assets/hero/7a.png";
-import hero8 from "../assets/hero/8a.png";
-import hero9 from "../assets/hero/9a.png";
-import hero10 from "../assets/hero/10a.png";
-import hero11 from "../assets/hero/11a.png";
-import hero12 from "../assets/hero/12a.png";
-import hero13 from "../assets/hero/13a.png";
-import hero14 from "../assets/hero/14a.png";
-import hero15 from "../assets/hero/15a.png";
-import hero16 from "../assets/hero/16a.png";
-import hero17 from "../assets/hero/17a.png";
-import hero18 from "../assets/hero/18a.png";
-import hero19 from "../assets/hero/19a.png";
-import hero20 from "../assets/hero/20a.png";
-import hero21 from "../assets/hero/21a.png";
-import hero22 from "../assets/hero/22a.png";
-import hero23 from "../assets/hero/23a.png";
-import hero24 from "../assets/hero/24a.png";
-import hero25 from "../assets/hero/25a.png";
-import hero26 from "../assets/hero/26a.png";
-import hero27 from "../assets/hero/27a.png";
-import hero28 from "../assets/hero/28a.png";
+// // Replace these with your actual image imports
+// import hero1 from "../assets/hero/1a.png";
+// import hero2 from "../assets/hero/2a.png";
+// import hero3 from "../assets/hero/3a.png";
+// import hero4 from "../assets/hero/4a.png";
+// import hero5 from "../assets/hero/5a.png";
+// import hero6 from "../assets/hero/6a.png";
+// import hero7 from "../assets/hero/7a.png";
+// import hero8 from "../assets/hero/8a.png";
+// import hero9 from "../assets/hero/9a.png";
+// import hero10 from "../assets/hero/10a.png";
+// import hero11 from "../assets/hero/11a.png";
+// import hero12 from "../assets/hero/12a.png";
+// import hero13 from "../assets/hero/13a.png";
+// import hero14 from "../assets/hero/14a.png";
+// import hero15 from "../assets/hero/15a.png";
+// import hero16 from "../assets/hero/16a.png";
+// import hero17 from "../assets/hero/17a.png";
+// import hero18 from "../assets/hero/18a.png";
+// import hero19 from "../assets/hero/19a.png";
+// import hero20 from "../assets/hero/20a.png";
+// import hero21 from "../assets/hero/21a.png";
+// import hero22 from "../assets/hero/22a.png";
+// import hero23 from "../assets/hero/23a.png";
+// import hero24 from "../assets/hero/24a.png";
+// import hero25 from "../assets/hero/25a.png";
+// import hero26 from "../assets/hero/26a.png";
+// import hero27 from "../assets/hero/27a.png";
+// import hero28 from "../assets/hero/28a.png";
 
+
+// export const heroSlides = [
+//   {
+//     id: 1,
+//     image: hero1,
+//     alt: "Air traffic control towers construction",
+//   },
+//   {
+//     id: 2,
+//     image: hero2,
+//     alt: "Airports Terminal buildings project",
+//   },
+//   {
+//     id: 3,
+//     image: hero3,
+//     alt: "Cargo terminals construction",
+//   },
+//   {
+//     id: 4,
+//     image: hero4,
+//     alt: "Building construction project",
+//   },
+//   {
+//     id: 5,
+//     image: hero5,
+//     alt: "Steel framed buildings construction",
+//   },
+//   {
+//     id: 6,
+//     image: hero6,
+//     alt: "Contact piers construction",
+//   },
+//   {
+//     id: 7,
+//     image: hero7,
+//     alt: "Drainage system installation",
+//   },
+//   {
+//     id: 8,
+//     image: hero8,
+//     alt: "General Civil Engineering Works",
+//   },
+//   {
+//     id: 9,
+//     image: hero9,
+//     alt: "Industrial plants with prefabricated components",
+//   },
+//   {
+//     id: 10,
+//     image: hero10,
+//     alt: "Solar Power LED Street Lighting System installation",
+//   },
+//   {
+//     id: 11,
+//     image: hero11,
+//     alt: "Oil and gas pipelines maintenance",
+//   },
+//   {
+//     id: 12,
+//     image: hero12,
+//     alt: "Irrigation & Flood Control Systems",
+//   },
+//   {
+//     id: 13,
+//     image: hero13,
+//     alt: "Irrigation system construction",
+//   },
+//   {
+//     id: 14,
+//     image: hero14,
+//     alt: "Flood Control Systems installation",
+//   },
+//   {
+//     id: 15,
+//     image: hero15,
+//     alt: "Low Voltage Installation",
+//   },
+//   {
+//     id: 16,
+//     image: hero16,
+//     alt: "Miscellaneous Specialized construction",
+//   },
+//   {
+//     id: 17,
+//     image: hero17,
+//     alt: "Passenger loading bridges installation",
+//   },
+//   {
+//     id: 18,
+//     image: hero18,
+//     alt: "Telecommunication Installations",
+//   },
+//   {
+//     id: 19,
+//     image: hero19,
+//     alt: "Solar Power installation",
+//   },
+//   {
+//     id: 20,
+//     image: hero20,
+//     alt: "Solar Installation project",
+//   },
+//   {
+//     id: 21,
+//     image: hero21,
+//     alt: "Solar Energy system installation",
+//   },
+//   {
+//     id: 22,
+//     image: hero22,
+//     alt: "Signcraft Installation",
+//   },
+//   {
+//     id: 23,
+//     image: hero23,
+//     alt: "Sewerage Works construction",
+//   },
+//   {
+//     id: 24,
+//     image: hero24,
+//     alt: "Security, Safety Surveillance System",
+//   },
+//   {
+//     id: 25,
+//     image: hero25,
+//     alt: "Satellite buildings construction",
+//   },
+//   {
+//     id: 26,
+//     image: hero26,
+//     alt: "Road and Pavements construction",
+//   },
+//   {
+//     id: 27,
+//     image: hero27,
+//     alt: "Retaining Structure project",
+//   },
+//   {
+//     id: 28,
+//     image: hero28,
+//     alt: "Pavements construction",
+//   }
+// ];
+
+
+// Keep only the video import
+// import heroVideo from "../assets/logos/favicon_io/v2.mp4"; // Replace with your actual video path
+
+// export const heroSlides = [
+//   {
+//     id: 1,
+//     type: "video",
+//     video: heroVideo,
+//     alt: "Construction projects showcase video",
+//   }
+//   // Remove all the image imports and objects
+// ];
+
+// https://drive.google.com/file/d/1pwmsFyTlu1FnvfjFR1Nap-C-UtTOqs-u/view?usp=drive_link
+
+// In your hero.js file
+// export const heroSlides = [
+//   {
+//     id: 1,
+//     type: "video",
+//     video: "https://drive.google.com/file/d/1pwmsFyTlu1FnvfjFR1Nap-C-UtTOqs-u/view?usp=drive_link", // External URL
+//     alt: "Construction projects showcase video",
+//   }
+// ];
+
+// Replace YOUR_FILE_ID with your actual file ID
+// Your file ID is: 1pwmsFyTlu1FnvfjFR1Nap-C-UtTOqs-u
 
 export const heroSlides = [
   {
     id: 1,
-    image: hero1,
-    alt: "Air traffic control towers construction",
-  },
-  {
-    id: 2,
-    image: hero2,
-    alt: "Airports Terminal buildings project",
-  },
-  {
-    id: 3,
-    image: hero3,
-    alt: "Cargo terminals construction",
-  },
-  {
-    id: 4,
-    image: hero4,
-    alt: "Building construction project",
-  },
-  {
-    id: 5,
-    image: hero5,
-    alt: "Steel framed buildings construction",
-  },
-  {
-    id: 6,
-    image: hero6,
-    alt: "Contact piers construction",
-  },
-  {
-    id: 7,
-    image: hero7,
-    alt: "Drainage system installation",
-  },
-  {
-    id: 8,
-    image: hero8,
-    alt: "General Civil Engineering Works",
-  },
-  {
-    id: 9,
-    image: hero9,
-    alt: "Industrial plants with prefabricated components",
-  },
-  {
-    id: 10,
-    image: hero10,
-    alt: "Solar Power LED Street Lighting System installation",
-  },
-  {
-    id: 11,
-    image: hero11,
-    alt: "Oil and gas pipelines maintenance",
-  },
-  {
-    id: 12,
-    image: hero12,
-    alt: "Irrigation & Flood Control Systems",
-  },
-  {
-    id: 13,
-    image: hero13,
-    alt: "Irrigation system construction",
-  },
-  {
-    id: 14,
-    image: hero14,
-    alt: "Flood Control Systems installation",
-  },
-  {
-    id: 15,
-    image: hero15,
-    alt: "Low Voltage Installation",
-  },
-  {
-    id: 16,
-    image: hero16,
-    alt: "Miscellaneous Specialized construction",
-  },
-  {
-    id: 17,
-    image: hero17,
-    alt: "Passenger loading bridges installation",
-  },
-  {
-    id: 18,
-    image: hero18,
-    alt: "Telecommunication Installations",
-  },
-  {
-    id: 19,
-    image: hero19,
-    alt: "Solar Power installation",
-  },
-  {
-    id: 20,
-    image: hero20,
-    alt: "Solar Installation project",
-  },
-  {
-    id: 21,
-    image: hero21,
-    alt: "Solar Energy system installation",
-  },
-  {
-    id: 22,
-    image: hero22,
-    alt: "Signcraft Installation",
-  },
-  {
-    id: 23,
-    image: hero23,
-    alt: "Sewerage Works construction",
-  },
-  {
-    id: 24,
-    image: hero24,
-    alt: "Security, Safety Surveillance System",
-  },
-  {
-    id: 25,
-    image: hero25,
-    alt: "Satellite buildings construction",
-  },
-  {
-    id: 26,
-    image: hero26,
-    alt: "Road and Pavements construction",
-  },
-  {
-    id: 27,
-    image: hero27,
-    alt: "Retaining Structure project",
-  },
-  {
-    id: 28,
-    image: hero28,
-    alt: "Pavements construction",
+    type: "video",
+    video: "https://res.cloudinary.com/iwzikyta/video/upload/v2_plgrng.mp4",
+    alt: "Construction projects showcase video",
   }
 ];
